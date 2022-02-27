cf4305c09e5ae0768d43a04502d836535050a3d8
pyrex8 <ev64bug@yahoo.com>
2022-02-27 13:07:12 -0800

    moved ROM into RAM, remved glitches

--- common/rom.c
+++ common/rom.c
@@ -10,16 +10,19 @@
 #include "rom_data.h"
 
 #define ROM_LOCATION  0xC000
+#define ROM_END 0xFFFF
+#define ROM_SIZE (ROM_END - ROM_LOCATION)
 #define ROM_RESET_VECTOR_LB_LOCATION  0xFFFC
 #define ROM_RESET_VECTOR_HB_LOCATION  0xFFFD
 
-static uint8_t rom_reset_vector_lb = 0;
-static uint8_t rom_reset_vector_hb = 0;
+static uint8_t rom_in_ram[ROM_SIZE];
 
 void rom_init(void)
 {
-    rom_reset_vector_lb = apple2e_enhanced_rom[ROM_RESET_VECTOR_LB_LOCATION - ROM_LOCATION];
-    rom_reset_vector_hb = apple2e_enhanced_rom[ROM_RESET_VECTOR_HB_LOCATION - ROM_LOCATION];
+    for (int i = 0; i < ROM_SIZE; i++)
+    {
+        rom_in_ram[i] = apple2e_enhanced_rom[i];
+    }
 }
 
 void rom_deinit(void)
@@ -30,22 +33,12 @@ void rom_update(uint8_t read, uint16_t address, uint8_t *byte)
 {
   if ((read) && (address >= ROM_LOCATION))
   {
-    *byte = apple2e_enhanced_rom[address - ROM_LOCATION];
-  }
-
-  if (address == ROM_RESET_VECTOR_LB_LOCATION)
-  {
-      *byte = rom_reset_vector_lb;
-  }
-
-  if (address == ROM_RESET_VECTOR_HB_LOCATION)
-  {
-      *byte = rom_reset_vector_hb;
+    *byte = rom_in_ram[address - ROM_LOCATION];
   }
 }
 
 void rom_reset_vector_write(uint8_t lower_byte, uint8_t upper_byte)
 {
-    rom_reset_vector_lb = lower_byte;
-    rom_reset_vector_hb = upper_byte;
+    rom_in_ram[ROM_RESET_VECTOR_LB_LOCATION] = lower_byte;
+    rom_in_ram[ROM_RESET_VECTOR_HB_LOCATION] = upper_byte;
 }
