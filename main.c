d459c4a0abb360c3546ca512db968cf0ee292b4f
pyrex8 <ev64bug@yahoo.com>
2022-03-05 11:46:23 -0800

    moved VGA hardware calls to module

--- main.c
+++ main.c
@@ -10,17 +10,7 @@
 #include "hardware/irq.h"
 #include "hardware/structs/mpu.h"
 #include "hardware/structs/vreg_and_chip_reset.h"
-#include "hardware/structs/bus_ctrl.h"
-#include "parallel.pio.h"
 
-#include "common/rom.h"
-#include "common/ram.h"
-#include "common/keyboard.h"
-#include "common/joystick.h"
-#include "common/audio.h"
-#include "common/video.h"
-#include "common/disk.h"
-#include "m6502/c6502.h"
 
 #include "mcu/clock.h"
 #include "mcu/led.h"
@@ -29,65 +19,16 @@
 #include "mcu/speaker.h"
 #include "mcu/vga.h"
 
-// Pixel freq 25.175MHz for VGA Signal 640 x 480 @ 60 Hz
-#define PCLK_DIVIDER_INTEGER 16
-#define PCLK_DIVIDER_FRACT 12
-#define PCLK_PWM_COUNT 1
-#define PCLK_PWM_VALUE 1
+#include "m6502/c6502.h"
 
-// Vertical refresh 31.46875 kHz
-#define VGA_H_VISIBLE_AREA 640
-#define VGA_H_FRONT_PORCH 16
-#define VGA_H_SYNC_PULSE 96
-#define VGA_H_BACK_PORCH 48
-#define VGA_H_WHOLE_LINE 800
+#include "common/rom.h"
+#include "common/ram.h"
+#include "common/keyboard.h"
+#include "common/joystick.h"
+#include "common/audio.h"
+#include "common/video.h"
+#include "common/disk.h"
 
-#define HSYNC_DIVIDER 1
-#define HSYNC__DIVIDER_INTEGER (PCLK_DIVIDER_INTEGER)
-#define HSYNC__DIVIDER_FRACT (PCLK_DIVIDER_FRACT)
-#define HSYNC_PWM_COUNT (VGA_H_WHOLE_LINE - 1)
-#define HSYNC_PWM_VALUE (VGA_H_WHOLE_LINE - VGA_H_SYNC_PULSE)
-
-// Screen refresh rate 60 Hz
-#define VGA_V_VISIBLE_AREA 480
-#define VGA_V_FRONT_PORCH 10
-#define VGA_V_SYNC_PULSE 2
-#define VGA_V_BACK_PORCH 33
-#define VGA_V_WHOLE_FRAME 525
-
-// // 800 / 8 = 100
-#define VSYNC_CLK_MULTIPLIER 8
-#define VSYNC_SCAN_MULTIPLIER (VGA_H_WHOLE_LINE / VSYNC_CLK_MULTIPLIER)
-
-#define VSYNC_DIVIDER_INTEGER 134 // 8 * (16 + 12 / 16)
-#define VSYNC_DIVIDER_FRACT 0
-#define VSYNC_PWM_COUNT (VGA_V_WHOLE_FRAME * VSYNC_SCAN_MULTIPLIER - 1)
-#define VSYNC_PWM_VALUE (VSYNC_SCAN_MULTIPLIER * (VGA_V_WHOLE_FRAME - VGA_V_SYNC_PULSE))
-
-#define VGA_TO_VIDEO_SCAN_LINES_DIVIDER 2
-#define VIDEO_SCAN_LINES (VGA_V_WHOLE_FRAME / VGA_TO_VIDEO_SCAN_LINES_DIVIDER)
-
-#define VIDEO_RESOLUTION_X 280
-#define VIDEO_RESOLUTION_Y 192
-#define VIDEO_BORDER_X ((VGA_H_VISIBLE_AREA / 2 - VIDEO_RESOLUTION_X) / 2)
-#define VIDEO_BORDER_Y ((VGA_V_VISIBLE_AREA / 2 - VIDEO_RESOLUTION_Y) / 2)
-
-#define VIDEO_SCAN_BUFFER_OFFSET ((VGA_H_BACK_PORCH + (VGA_H_VISIBLE_AREA - VIDEO_RESOLUTION_X * 2) / 2) / 2) //44
-#define VIDEO_SCAN_LINE_OFFSET ((VGA_V_BACK_PORCH + (VGA_V_VISIBLE_AREA - VIDEO_RESOLUTION_Y * 2) / 2))   //40.5 rounded down
-
-// one last value of zero otherwise last pixel repeats to the end of the scan line
-#define VIDEO_SCAN_BUFFER_LEN ((VGA_H_BACK_PORCH + VGA_H_VISIBLE_AREA) / 2 + 1)
-#define VIDEO_SCAN_LINE_LEN (VGA_H_WHOLE_LINE / 2)
-
-#define VIDEO_BYTES_PER_LINE 40
-
-#define VIDEO_SEGMENT_OFFSET 0x28
-#define SCREEN_LINE_OFFSET 0x80
-
-const uint VSYNC_PIN = 17;
-const uint HSYNC_PIN = 19;
-const uint PCLK_PIN = 5;
-const uint R0_PIN = 0;
 
 typedef enum
 {
@@ -114,28 +55,9 @@ static C6502_interface interface_c;
 static uint8_t video_line_data[VIDEO_BYTES_PER_LINE] = {0};
 static uint16_t video_data_address = 0x2000;
 
-PIO pio;
-uint offset;
-uint sm;
-int pio_dma_chan;
-
-uint hsync_slice;
-uint hsync_channel;
-
-uint vsync_slice;
-uint vsync_channel;
-
-int pclk_slice;
-uint pclk_channel;
-
 uint16_t scan_line;
-int16_t overscan_line;
+uint16_t overscan_line;
 uint8_t overscan_line_odd;
-uint16_t h_pixel;
-
-uint16_t scan_line_buffer[VIDEO_SCAN_LINE_LEN] = {0};
-uint16_t scan_line_blank[VIDEO_SCAN_LINE_LEN] = {0};
-uint16_t *p_scan_line_buffer;
 
 static SerialMode serial_loader = SERIAL_READY;
 static UserState user_state = SERIAL_USER_KEYBOARD;
@@ -280,56 +202,6 @@ void uart_data(void)
             }
         }
     }
-    // Note: Call -1184
-}
-
-void __attribute__((noinline, long_call, section(".time_critical"))) vga_scan_line(void)
-{
-//    dma_channel_set_read_addr(pio_dma_chan, scan_line_buffer, true);
-
-    test0_pin_high();
-
-    overscan_line = pwm_get_counter(vsync_slice) / VSYNC_SCAN_MULTIPLIER - VIDEO_SCAN_LINE_OFFSET;
-    overscan_line_odd = overscan_line & 0x01;
-
-    if (overscan_line >= 0)
-    {
-        scan_line = overscan_line / 2;
-    }
-    else
-    {
-        scan_line = VIDEO_SCAN_LINES + overscan_line / 2;
-        video_scan_line_set(scan_line);
-    }
-
-    if (overscan_line_odd)
-    {
-        video_buffer_get(&scan_line_buffer[VIDEO_SCAN_BUFFER_OFFSET]);
-    }
-    else
-    {
-        scan_line_ram_read();
-    }
-
-    pwm_clear_irq(hsync_slice);
-
-    if (scan_line >= VIDEO_RESOLUTION_Y)
-    {
-        memcpy(scan_line_buffer, scan_line_blank, VIDEO_SCAN_BUFFER_LEN);
-    }
-
-    uart_data();
-
-    if (disk_spinning_test())
-    {
-        led_red_high();
-    }
-    else
-    {
-        led_red_low();
-    }
-
-    test0_pin_low();
 }
 
 void core1_main(void)
@@ -372,82 +244,60 @@ int main(void)
 
     multicore_launch_core1(core1_main);
 
-    bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
+    vga_init();
 
-    p_scan_line_buffer = scan_line_buffer;
-
-    pio = pio0;
-    offset = pio_add_program(pio, &parallel_program);
-    sm = pio_claim_unused_sm(pio, true);
-    parallel_program_init(pio, sm, offset, R0_PIN);
-
-    gpio_set_function(HSYNC_PIN, GPIO_FUNC_PWM);
-    hsync_slice = pwm_gpio_to_slice_num(HSYNC_PIN);
-    hsync_channel = pwm_gpio_to_channel(HSYNC_PIN);
-
-    pwm_clear_irq(hsync_slice);
-    pwm_set_irq_enabled(hsync_slice, true);
-    irq_set_exclusive_handler(PWM_IRQ_WRAP, vga_scan_line);
-    irq_set_priority(PWM_IRQ_WRAP, 0);
-    irq_set_enabled(PWM_IRQ_WRAP, true);
-
-    pwm_set_clkdiv_int_frac (hsync_slice, HSYNC__DIVIDER_INTEGER, HSYNC__DIVIDER_FRACT);
-    pwm_set_wrap(hsync_slice, HSYNC_PWM_COUNT);
-    pwm_set_chan_level(hsync_slice, hsync_channel, HSYNC_PWM_VALUE);
-
-    gpio_set_function(VSYNC_PIN, GPIO_FUNC_PWM);
-    vsync_slice = pwm_gpio_to_slice_num(VSYNC_PIN);
-    vsync_channel = pwm_gpio_to_channel(VSYNC_PIN);
-    pwm_set_clkdiv_int_frac (vsync_slice, VSYNC_DIVIDER_INTEGER, VSYNC_DIVIDER_FRACT);
-    pwm_set_wrap(vsync_slice, VSYNC_PWM_COUNT);
-    pwm_set_chan_level(vsync_slice, vsync_channel, VSYNC_PWM_VALUE);
-
-    gpio_set_function(PCLK_PIN, GPIO_FUNC_PWM);
-    pclk_slice = pwm_gpio_to_slice_num(PCLK_PIN);
-    pclk_channel = pwm_gpio_to_channel(PCLK_PIN);
-    pwm_set_clkdiv_int_frac (pclk_slice, PCLK_DIVIDER_INTEGER, PCLK_DIVIDER_FRACT);
-    pwm_set_wrap(pclk_slice, PCLK_PWM_COUNT);
-    pwm_set_chan_level(pclk_slice, pclk_channel, PCLK_PWM_VALUE);
-
-    int ctrl_chan = dma_claim_unused_channel(true);
-    pio_dma_chan = dma_claim_unused_channel(true);
-
-     // Setup the control channel
-     dma_channel_config c = dma_channel_get_default_config(ctrl_chan); // default configs
-     channel_config_set_transfer_data_size(&c, DMA_SIZE_32); // 32-bit txfers
-     channel_config_set_read_increment(&c, false); // no read incrementing
-     channel_config_set_write_increment(&c, false); // no write incrementing
-     channel_config_set_chain_to(&c, pio_dma_chan);
-     channel_config_set_dreq(&c, DREQ_PWM_WRAP0 + pclk_slice);
-
-     dma_channel_configure(
-         ctrl_chan,
-         &c,
-         &dma_hw->ch[pio_dma_chan].al3_read_addr_trig,
-         &p_scan_line_buffer,
-         1,
-         false
-     );
-
-    dma_channel_config pio_dma_chan_config = dma_channel_get_default_config(pio_dma_chan);
-
-    channel_config_set_transfer_data_size(&pio_dma_chan_config, DMA_SIZE_16);
-    channel_config_set_read_increment(&pio_dma_chan_config, true);
-    channel_config_set_write_increment(&pio_dma_chan_config, false);
-    channel_config_set_chain_to(&pio_dma_chan_config, ctrl_chan);
-    channel_config_set_dreq(&pio_dma_chan_config, DREQ_PWM_WRAP0 + pclk_slice);
-
-    dma_channel_configure(
-        pio_dma_chan,
-        &pio_dma_chan_config,
-        &pio->txf[sm],
-        scan_line_buffer,
-        VIDEO_SCAN_LINE_LEN - 1,
-        true);
-
-    pwm_set_mask_enabled ((1 << hsync_slice) | (1 << vsync_slice) | (1 << pclk_slice));
+    bool scan_line_nrdy = true;
 
     while (1)
     {
+        scan_line_nrdy = vga_scan_line_not_ready();
+        while(scan_line_nrdy)
+        {
+            scan_line_nrdy = vga_scan_line_not_ready();
+        }
+        vga_scan_line_not_ready_reset();
+
+        test0_pin_high();
+
+        overscan_line = vga_overscan_line_get();
+        overscan_line_odd = overscan_line & 0x01;
+
+        if (overscan_line >= 0)
+        {
+            scan_line = overscan_line / 2;
+        }
+        else
+        {
+            scan_line = VIDEO_SCAN_LINES + overscan_line / 2;
+            video_scan_line_set(scan_line);
+        }
+
+        if (overscan_line_odd)
+        {
+            video_buffer_get(vga_scan_line_buffer());
+        }
+        else
+        {
+            scan_line_ram_read();
+        }
+
+
+        if (scan_line >= VIDEO_RESOLUTION_Y)
+        {
+            vga_blank_scan_line_set();
+        }
+
+        uart_data();
+
+        if (disk_spinning_test())
+        {
+            led_red_high();
+        }
+        else
+        {
+            led_red_low();
+        }
+
+        test0_pin_low();
     }
 }
--- mcu/vga.c
+++ mcu/vga.c
@@ -1,2 +1,151 @@
+#include <string.h>
+#include <stdint.h>
+
 #include "vga.h"
+
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/binary_info.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "hardware/pwm.h"
+#include "hardware/dma.h"
+#include "hardware/irq.h"
+#include "hardware/structs/mpu.h"
+#include "hardware/structs/vreg_and_chip_reset.h"
+#include "parallel.pio.h"
+
+const uint VSYNC_PIN = 17;
+const uint HSYNC_PIN = 19;
+const uint PCLK_PIN = 5;
+const uint R0_PIN = 0;
+
+PIO pio;
+uint offset;
+uint sm;
+int pio_dma_chan;
+
+uint hsync_slice;
+uint hsync_channel;
+
+uint vsync_slice;
+uint vsync_channel;
+
+int pclk_slice;
+uint pclk_channel;
+
+static uint16_t scan_line_buffer[VIDEO_SCAN_LINE_LEN] = {0};
+static uint16_t scan_line_blank[VIDEO_SCAN_LINE_LEN] = {0};
+static uint16_t *p_scan_line_buffer;
+static bool scan_line_old = true;
+
+
+void __attribute__((noinline, long_call, section(".time_critical"))) vga_scan_line(void)
+{
+    pwm_clear_irq(hsync_slice);
+    scan_line_old = false;
+}
+
+void vga_init(void)
+{
+//    bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
+
+    p_scan_line_buffer = scan_line_buffer;
+
+    pio = pio0;
+    offset = pio_add_program(pio, &parallel_program);
+    sm = pio_claim_unused_sm(pio, true);
+    parallel_program_init(pio, sm, offset, R0_PIN);
+
+    gpio_set_function(HSYNC_PIN, GPIO_FUNC_PWM);
+    hsync_slice = pwm_gpio_to_slice_num(HSYNC_PIN);
+    hsync_channel = pwm_gpio_to_channel(HSYNC_PIN);
+
+    pwm_clear_irq(hsync_slice);
+    pwm_set_irq_enabled(hsync_slice, true);
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, vga_scan_line);
+    irq_set_priority(PWM_IRQ_WRAP, 0);
+    irq_set_enabled(PWM_IRQ_WRAP, true);
+
+    pwm_set_clkdiv_int_frac (hsync_slice, HSYNC__DIVIDER_INTEGER, HSYNC__DIVIDER_FRACT);
+    pwm_set_wrap(hsync_slice, HSYNC_PWM_COUNT);
+    pwm_set_chan_level(hsync_slice, hsync_channel, HSYNC_PWM_VALUE);
+
+    gpio_set_function(VSYNC_PIN, GPIO_FUNC_PWM);
+    vsync_slice = pwm_gpio_to_slice_num(VSYNC_PIN);
+    vsync_channel = pwm_gpio_to_channel(VSYNC_PIN);
+    pwm_set_clkdiv_int_frac (vsync_slice, VSYNC_DIVIDER_INTEGER, VSYNC_DIVIDER_FRACT);
+    pwm_set_wrap(vsync_slice, VSYNC_PWM_COUNT);
+    pwm_set_chan_level(vsync_slice, vsync_channel, VSYNC_PWM_VALUE);
+
+    gpio_set_function(PCLK_PIN, GPIO_FUNC_PWM);
+    pclk_slice = pwm_gpio_to_slice_num(PCLK_PIN);
+    pclk_channel = pwm_gpio_to_channel(PCLK_PIN);
+    pwm_set_clkdiv_int_frac (pclk_slice, PCLK_DIVIDER_INTEGER, PCLK_DIVIDER_FRACT);
+    pwm_set_wrap(pclk_slice, PCLK_PWM_COUNT);
+    pwm_set_chan_level(pclk_slice, pclk_channel, PCLK_PWM_VALUE);
+
+    int ctrl_chan = dma_claim_unused_channel(true);
+    pio_dma_chan = dma_claim_unused_channel(true);
+
+     // Setup the control channel
+     dma_channel_config c = dma_channel_get_default_config(ctrl_chan); // default configs
+     channel_config_set_transfer_data_size(&c, DMA_SIZE_32); // 32-bit txfers
+     channel_config_set_read_increment(&c, false); // no read incrementing
+     channel_config_set_write_increment(&c, false); // no write incrementing
+     channel_config_set_chain_to(&c, pio_dma_chan);
+     channel_config_set_dreq(&c, DREQ_PWM_WRAP0 + pclk_slice);
+
+     dma_channel_configure(
+         ctrl_chan,
+         &c,
+         &dma_hw->ch[pio_dma_chan].al3_read_addr_trig,
+         &p_scan_line_buffer,
+         1,
+         false
+     );
+
+    dma_channel_config pio_dma_chan_config = dma_channel_get_default_config(pio_dma_chan);
+
+    channel_config_set_transfer_data_size(&pio_dma_chan_config, DMA_SIZE_16);
+    channel_config_set_read_increment(&pio_dma_chan_config, true);
+    channel_config_set_write_increment(&pio_dma_chan_config, false);
+    channel_config_set_chain_to(&pio_dma_chan_config, ctrl_chan);
+    channel_config_set_dreq(&pio_dma_chan_config, DREQ_PWM_WRAP0 + pclk_slice);
+
+    dma_channel_configure(
+        pio_dma_chan,
+        &pio_dma_chan_config,
+        &pio->txf[sm],
+        scan_line_buffer,
+        VIDEO_SCAN_LINE_LEN - 1,
+        true);
+
+    pwm_set_mask_enabled ((1 << hsync_slice) | (1 << vsync_slice) | (1 << pclk_slice));
+}
+
+uint16_t vga_overscan_line_get(void)
+{
+    return pwm_get_counter(vsync_slice) / VSYNC_SCAN_MULTIPLIER - VIDEO_SCAN_LINE_OFFSET;
+}
+
+uint16_t *vga_scan_line_buffer(void)
+{
+    return &scan_line_buffer[VIDEO_SCAN_BUFFER_OFFSET];
+}
+
+void vga_blank_scan_line_set(void)
+{
+    memcpy(scan_line_buffer, scan_line_blank, VIDEO_SCAN_BUFFER_LEN);
+}
+
+bool vga_scan_line_not_ready(void)
+{
+    return scan_line_old;
+}
+
+bool vga_scan_line_not_ready_reset(void)
+{
+    scan_line_old = true;
+
+}
