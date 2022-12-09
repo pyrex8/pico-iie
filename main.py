1fd177b7e35ee6092d0ca127519e9ec9e8766472
pyrex8 <ev64bug@yahoo.com>
2022-12-09 15:18:17 -0800

    main.py is now just for loading binary file

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python3
 """
-pico-iie emulator in C and pygame
+pico-iie emulator binary downloader
 """
 
 import os
 import time
-import pygame
-import pygame.sndarray
-import pygame.surfarray
 import numpy
 import sys
 import serial
 
 bin_name = ""
-
-PIXELS_X = 280
-PIXELS_Y = 192
-SCREEN_SCALE = 4
-SCREEN_X = PIXELS_X * SCREEN_SCALE
-SCREEN_Y = PIXELS_Y * SCREEN_SCALE
-SCREEN_X_TOTAL = SCREEN_X
-SCREEN_Y_TOTAL = SCREEN_Y
 
 SERIAL_BIN_DATA = 0x82
 SERIAL_SIZE_LSB = 0x83
@@ -29,11 +18,6 @@ SERIAL_ADDR_LSB = 0x85
 SERIAL_ADDR_MSB = 0x86
 SERIAL_RESET = 0x87
 SERIAL_REBOOT = 0x88
-
-BLACK = (0, 0, 0)
-
-dest = r'~!@#$%^&*()_+{}|:"<>?'
-src =  r"`1234567890-=[]\;',./"
 
 BAUDRATE = 230400
 COM_PORT = '/dev/ttyUSB0'
@@ -60,7 +44,7 @@ if bin_name != "":
 
     bin_image = open(bin_name, 'rb')
 
-    file_size = hex(os.path.getsize(bin_name))
+    file_size = hex(os.path.getsize(bin_name) - 1)
     file_size_lsb = int(file_size[-2:], 16)
     file_size_msb = int(file_size[-4:-2], 16)
 
@@ -116,109 +100,6 @@ if bin_name != "":
 
     print('bin file name:', bin_name)
     print('bin file length:',bin_file_len, 'bytes')
-
-joystick_present = 0
-joystick_buttons = 0
-button_0 = 0
-button_1 = 0
-paddle_0 = 0
-paddle_1 = 0
-
-pygame.init()
-pygame.display.set_caption('pyrex8')
-
-try:
-    icon = pygame.image.load('icon.png')
-    pygame.display.set_icon(icon)
-except:
-    pass
-
-screen = pygame.display.set_mode([SCREEN_X_TOTAL, SCREEN_Y_TOTAL])
-clock = pygame.time.Clock()
-
-running = True
-
-while running:
-
-    if joystick_present:
-        button_0 = int(joystick.get_button(0))
-        if joystick_buttons > 1:
-            button_1 = int(joystick.get_button(1))
-        paddle_0 = int((joystick.get_axis(0) + 1) * 127)
-        paddle_1 = int((joystick.get_axis(1) + 1) * 127)
-    else:
-        if pygame.joystick.get_count() > 0:
-            joystick = pygame.joystick.Joystick(0)
-            joystick.init()
-            joystick_present = 1
-            joystick_buttons = joystick.get_numbuttons()
-        button_0 = 0
-        button_1 = 0
-        paddle_0 = 128
-        paddle_1 = 128
-
-    keycode = 0
-
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            running = False
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_F10:
-                running = False
-
-            if event.key == pygame.K_F1:
-                keycode = 128
-
-            if event.key == pygame.K_F2:
-                keycode = 129
-
-            if event.key == pygame.K_F9:
-                keycode = 130
-
-            if event.key < 0x7F:
-                keycode = event.key
-                mod = pygame.key.get_mods() #KMOD_SHIFT, #KMOD_CAPS
-
-                if keycode >= 0x61 and keycode <= 0x7A and (mod & pygame.KMOD_CAPS) == 0:
-                    keycode = keycode - 0x20
-
-                ch = chr(event.key)
-                pressed = pygame.key.get_pressed()
-                if pressed[pygame.K_RSHIFT] or pressed[pygame.K_LSHIFT] and ch in src:
-                    ch = dest[src.index(ch)]
-                    keycode = ord(ch)
-
-                keyctrl = event.key
-                if (pressed[pygame.K_RCTRL] or pressed[pygame.K_LCTRL]) and keyctrl >= 0x61 and keyctrl <= 0x7A:
-                    keycode = keyctrl - 0x60
-
-            if event.key == pygame.K_UP:
-                keycode = 0x0B
-            if event.key == pygame.K_DOWN:
-                keycode = 0x0A
-            if event.key == pygame.K_LEFT:
-                keycode = 0x08
-            if event.key == pygame.K_RIGHT:
-                keycode = 0x15
-
-    to_send = bytes([keycode])
-    packet = bytearray()
-    packet.append(0x81)
-    packet.append(keycode)
-    packet.append(button_0)
-    packet.append(button_1)
-    packet.append(paddle_0)
-    packet.append(paddle_1)
-    ser.write(packet)
-
-    pygame.draw.rect(screen, BLACK, (0, 0, SCREEN_X_TOTAL, SCREEN_Y_TOTAL))
-    pygame.display.flip()
-
-    clock.tick(60)
-
-pygame.quit()
 
 ser.flush()
 ser.close()
