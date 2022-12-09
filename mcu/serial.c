9f8fc2592785507ed162943005dbc9b7bc0fda5d
pyrex8 <ev64bug@yahoo.com>
2022-12-09 15:25:16 -0800

    removed serial keyboard and joystick functions

--- mcu/serial.c
+++ mcu/serial.c
@@ -3,8 +3,7 @@
 
 typedef enum
 {
-    SERIAL_READY = 0x80,
-    SERIAL_USER = 0x81,
+    SERIAL_READY = 0x81,
     SERIAL_BIN = 0x82,
     SERIAL_SIZE_LSB = 0x83,
     SERIAL_SIZE_MSB = 0x84,
@@ -14,17 +13,7 @@ typedef enum
     SERIAL_REBOOT = 0x88,
 } SerialMode;
 
-typedef enum
-{
-    SERIAL_USER_KEYBOARD = 0,
-    SERIAL_USER_BTN_0,
-    SERIAL_USER_BTN_1,
-    SERIAL_USER_JOY_X,
-    SERIAL_USER_JOY_Y,
-} UserState;
-
 static SerialMode serial_loader = SERIAL_READY;
-static UserState user_state = SERIAL_USER_KEYBOARD;
 static uint16_t bin_data_length = 0;
 static uint16_t bin_data_counter = 0;
 
@@ -32,7 +21,6 @@ static uint8_t game_x = 0;
 static uint8_t game_y = 0;
 static uint8_t button_0 = 0;
 static uint8_t button_1 = 0;
-
 
 void serial_init(void)
 {
@@ -50,52 +38,7 @@ void serial_update(SerialOperation *operation, uint8_t *data)
     {
         serial_byte = uart_getc(UART_ID);
 
-        if(serial_loader == SERIAL_USER)
-        {
-            // 5 bytes keyboard, button_0, button_1, paddle_0, paddle_1
-            if (user_state == SERIAL_USER_KEYBOARD)
-            {
-                if (serial_byte > 0 && serial_byte < 128)
-                {
-                    *operation = SERIAL_KEYBOARD_CODE;
-                }
-                else if (serial_byte == 128)
-                {
-                    *operation = SERIAL_MAIN_RESET;
-                }
-                else if (serial_byte == 129)
-                {
-                    *operation = SERIAL_MAIN_PAUSE;
-                }
-                else if (serial_byte == 130)
-                {
-                    *operation = SERIAL_MAIN_REBOOT;
-                }
-                user_state++;
-            }
-            else if (user_state == SERIAL_USER_BTN_0)
-            {
-                user_state++;
-                *operation = SERIAL_GAME_BTN0;
-            }
-            else if (user_state == SERIAL_USER_BTN_1)
-            {
-                user_state++;
-                *operation = SERIAL_GAME_BTN1;
-            }
-            else if (user_state == SERIAL_USER_JOY_X)
-            {
-                user_state++;
-                *operation = SERIAL_GAME_PDL0;
-            }
-            else
-            {
-                user_state = SERIAL_USER_KEYBOARD;
-                serial_loader = SERIAL_READY;
-                *operation = SERIAL_GAME_PDL1;
-            }
-        }
-        else if(serial_loader == SERIAL_BIN)
+        if(serial_loader == SERIAL_BIN)
         {
             // 48k = 0xC000
             if (bin_data_counter >= bin_data_length)
@@ -138,13 +81,7 @@ void serial_update(SerialOperation *operation, uint8_t *data)
         }
         else if(serial_loader == SERIAL_READY)
         {
-            if(serial_byte == SERIAL_USER)
-            {
-                serial_loader = SERIAL_USER;
-                user_state = SERIAL_USER_KEYBOARD;
-                *operation = SERIAL_MAIN_NULL;
-            }
-            else if(serial_byte == SERIAL_BIN)
+            if(serial_byte == SERIAL_BIN)
             {
                 serial_loader = SERIAL_BIN;
                 bin_data_counter = 0;
