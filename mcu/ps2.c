3b0fb0a7361b8df0647f66f39f0133a938451bed
pyrex8 <ev64bug@yahoo.com>
2022-12-08 17:57:16 -0800

    ps/2 keyboard partially working

--- mcu/ps2.c
+++ mcu/ps2.c
@@ -7,16 +7,66 @@
 #define PS2_DATA_PIN 16
 #define PS2_SCK_PIN 18
 
+#define PS2_ASCII_MAP_SIZE 0x77
+#define PS2_SHIFT_KEY 0x12
+#define PS2_CTRL_KEY 0x14
+#define PS2_CAPLOCK_KEY 0x58
+#define ASCII_LOWER_CASE_OFFSET 0x20
+
+#define PS2_UP_KEY 0x75
+#define PS2_DOWN_KEY 0x72
+#define PS2_LEFT_KEY 0x6B
+#define PS2_RIGHT_KEY 0x74
+#define PS2_F12_KEY 0x07
+
+#define A2E_UP_KEY 0x0B
+#define A2E_DOWN_KEY 0x0A
+#define A2E_LEFT_KEY 0x08
+#define A2E_RIGHT_KEY 0x15
+
 static bool ps2_keyboard_data_ready = false;
 static uint8_t ps2_keyboard_data = 0;
 
 static uint8_t bit_value = 0;
 static uint8_t bit_position = 0;
 static uint8_t data_raw = 0;
-static uint8_t data_value = 0;
 static bool key_up_detected = false;
 static uint8_t clk_state = 1;
 static uint8_t clk_state_last = 1;
+
+static uint8_t ps2_data = 0;
+static uint8_t ps2_key_up = 0;
+static uint8_t ps2_shift = 0;
+static uint8_t ps2_ctrl = 0;
+static uint8_t ps2_caplock = 0;
+
+static uint8_t key_code = 0;
+
+static const uint8_t keymap_ps2_2_iie[PS2_ASCII_MAP_SIZE] =
+{
+//0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x60, 0x00, //0x00
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x51, 0x31, 0x00, 0x00, 0x00, 0x5A, 0x53, 0x41, 0x57, 0x32, 0x00, //0x10
+  0x00, 0x43, 0x58, 0x44, 0x45, 0x34, 0x33, 0x00, 0x00, 0x20, 0x56, 0x46, 0x54, 0x52, 0x35, 0x00, //0x20
+  0x00, 0x4E, 0x42, 0x48, 0x47, 0x59, 0x36, 0x00, 0x00, 0x00, 0x4D, 0x4A, 0x55, 0x37, 0x38, 0x00, //0x30
+  0x00, 0x2C, 0x4B, 0x49, 0x4F, 0x30, 0x39, 0x00, 0x00, 0x2E, 0x2F, 0x4C, 0x3B, 0x50, 0x2D, 0x00, //0x40
+  0x00, 0x00, 0x27, 0x0,  0x5B, 0x3D, 0x00, 0x00, 0x00, 0x00, 0x0D, 0x5D, 0x00, 0x5C, 0x00, 0x00, //0x50
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, //0x60
+  0x00, 0x00, 0x0A, 0x00, 0x15, 0x0B, 0x1B,                                                       //0x70
+};
+
+static const uint8_t keymap_iie_shift[PS2_ASCII_MAP_SIZE] =
+{
+//0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x7E, 0x00, //0x00
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x51, 0x21, 0x00, 0x00, 0x00, 0x5A, 0x53, 0x41, 0x57, 0x40, 0x00, //0x10
+  0x00, 0x43, 0x58, 0x44, 0x45, 0x24, 0x23, 0x00, 0x00, 0x20, 0x56, 0x46, 0x54, 0x52, 0x25, 0x00, //0x20
+  0x00, 0x4E, 0x42, 0x48, 0x47, 0x59, 0x5E, 0x00, 0x00, 0x00, 0x4D, 0x4A, 0x55, 0x26, 0x2A, 0x00, //0x30
+  0x00, 0x3C, 0x4B, 0x49, 0x4F, 0x29, 0x28, 0x00, 0x00, 0x3E, 0x3F, 0x4C, 0x3A, 0x50, 0x5F, 0x00, //0x40
+  0x00, 0x00, 0x22, 0x0,  0x7B, 0x2B, 0x00, 0x00, 0x00, 0x00, 0x0D, 0x7D, 0x00, 0x7C, 0x00, 0x00, //0x50
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, //0x60
+  0x00, 0x00, 0x0A, 0x00, 0x15, 0x0B, 0x1B,                                                       //0x70
+};
 
 void ps2_init(void)
 {
@@ -41,41 +91,75 @@ void ps2_update(void)
                 }
                 if (bit_position == 8)
                 {
-                    data_value = data_raw;
+                    ps2_data = data_raw;
                 }
             }
             bit_position++;
+            // PS/2 Keyboard data is 11 bits long
+            // start (0) + bytes (LSB first) + parity bit (odd) + stop (1)
             if (bit_position > 10)
             {
                 bit_position = 0;
-                if (data_value == 0xF0)
+
+                if(ps2_data == 0xF0)
                 {
-                    key_up_detected = true;
-                }
-                else if (key_up_detected)
-                {
-                    key_up_detected = false;
+                  ps2_key_up = 1;
+                // TODO: fix this logic
+                  ps2_shift = 0;
+                  ps2_ctrl = 0;
                 }
                 else
                 {
-                    ps2_keyboard_data_ready = true;
+                    if(ps2_key_up == 0)
+                    {
+                        if(ps2_data == PS2_SHIFT_KEY)
+                        {
+                            ps2_shift = 1;
+                        }
+                        if(ps2_data == PS2_CTRL_KEY)
+                        {
+                            ps2_ctrl = 1;
+                        }
+
+                        if(ps2_data < PS2_ASCII_MAP_SIZE)
+                        {
+                            key_code = keymap_ps2_2_iie[ps2_data];
+                            if(ps2_data == PS2_CAPLOCK_KEY)
+                            {
+                                ps2_caplock = 1 - ps2_caplock;
+                            }
+                            // shift alphabet only NOTE:still not working
+                            if(ps2_caplock && key_code > 0x40 && key_code < 0x5B)
+                            {
+                                key_code += 0x20;
+                            }
+
+                            if(ps2_shift)
+                            {
+                                key_code = keymap_iie_shift[ps2_data];
+                            }
+
+                            if(ps2_ctrl)
+                            {
+                                if(ps2_data == PS2_F12_KEY)
+                                {
+                                    // reset = true;
+                                }
+                            }
+                        }
+                    }
+                    ps2_key_up = 0;
                 }
             }
         }
     }
-
-    // read data
-
-    // PS/2 Keyboard data is 11 bits long
-    // start (0) + bytes (LSB first) + parity bit (odd) + stop (1)
     clk_state_last = clk_state;
 }
 
 bool ps2_data_ready(void)
 {
-    if (ps2_keyboard_data_ready)
+    if (key_code > 0)
     {
-        ps2_keyboard_data_ready = false;
         return true;
     }
     return false;
@@ -83,6 +167,7 @@ bool ps2_data_ready(void)
 
 uint8_t ps2_data_get(void)
 {
-    return 65;
-//    return ps2_keyboard_data;
+    uint8_t rtn_keycode = key_code;
+    key_code = 0;
+    return rtn_keycode;
 }
