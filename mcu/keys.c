ebdec258da0f059c70977c47830fce0694295325
pyrex8 <ev64bug@yahoo.com>
2023-01-24 09:11:37 -0800

    debounce for test key working

--- mcu/keys.c
+++ mcu/keys.c
@@ -7,12 +7,14 @@
 #define KEYS_DATA_PIN 16
 #define KEYS_SCK_PIN 18
 #define KEYS_MATRIX_MASK 0x7F
-#define KEYS_MATRIX_COMBINATIONS (KEYS_MATRIX_MASK + 1)
+#define KEYS_MATRIX_TOTAL (KEYS_MATRIX_MASK + 1)
 
 uint8_t keys_clk_state = 1;
 uint8_t keys_index = 0;
 uint8_t keys_zeros = 0;
-uint8_t keys_data[KEYS_MATRIX_COMBINATIONS] = {0};
+uint8_t keys_data[KEYS_MATRIX_TOTAL] = {0};
+uint8_t keys_pressed[KEYS_MATRIX_TOTAL] = {0};
+uint8_t keys_used[KEYS_MATRIX_TOTAL] = {0};
 
 void keys_init(void)
 {
@@ -44,15 +46,23 @@ void keys_update(void)
     keys_clk_low();
 
     // 16 zeros in a row marks the end of the keyboard matrix scan
-    if (gpio_get(KEYS_DATA_PIN) == 0)
+    uint8_t key_test = gpio_get(KEYS_DATA_PIN) == 0? 1 : 0;
+    if (key_test)
     {
         keys_zeros++;
-        keys_data[keys_index] = 0;
+        // debounce with two valid scans in a row
+        if (keys_data[keys_index])
+        {
+            keys_pressed[keys_index] = key_test;
+        }
+        keys_data[keys_index] = key_test;
     }
     else
     {
         keys_zeros = 0;
-        keys_data[keys_index] = 1;
+        keys_pressed[keys_index] = key_test;
+        keys_used[keys_index] = key_test;
+        keys_data[keys_index] = key_test;
     }
 
     if (keys_zeros == 16)
@@ -67,9 +77,9 @@ void keys_update(void)
 
 uint8_t keys_data_get(void)
 {
-    if (keys_data[0x21] == 0)
+    if (keys_pressed[0x21] && keys_used[0x21] == 0)
     {
-        keys_data[0x21] = 0;
+        keys_used[0x21] = 1;
         return 1;
     }
     return 0;
