10f95edb53ad676d38479b470a7f929784a7f392
pyrex8 <ev64bug@yahoo.com>
2023-01-25 15:32:15 -0800

    shift and caps lock

--- mcu/keys.c
+++ mcu/keys.c
@@ -10,6 +10,7 @@
 #define KEYS_MATRIX_TOTAL (KEYS_MATRIX_MASK + 1)
 #define KEYS_MATRIX_VALID (KEYS_MATRIX_TOTAL - 0x10)
 
+// Apple IIe keyboard codes
 #define UP   0x0B
 #define DOWN 0x0A
 #define LEFT 0x08
@@ -19,11 +20,19 @@
 #define ESC  0x1B
 #define DEL  0x7F
 
+// Matrix scan codes index
 #define KEYS_SHIFT_LEFT 0x10
 #define KEYS_SHIFT_RIGHT 0x1B
 #define KEYS_CTRL_LEFT 0x41
 #define KEYS_CTRL_RIGHT 0x09
 #define KEYS_CAPS_LOCK 0x20
+#define KEYS_F1 0x00 // pause/resume
+#define KEYS_F2 0x42 // shift + f2 = reset
+#define KEYS_F3 0x43 // ctrl + f3 = reboot
+
+#define KEYS_OFFSET_CAPS_LOCK_ON  0x00
+#define KEYS_OFFSET_CAPS_LOCK_OFF 0x70
+#define KEYS_OFFSET_CAPS_SHIFT    0xE0
 
 uint8_t keys_clk_state = 1;
 uint8_t keys_index = 0;
@@ -32,13 +41,18 @@ uint8_t keys_ones = 0;
 uint8_t keys_data[KEYS_MATRIX_TOTAL] = {0};
 uint8_t keys_pressed[KEYS_MATRIX_TOTAL] = {0};
 uint8_t keys_used[KEYS_MATRIX_TOTAL] = {0};
+uint8_t keys_iie_offset = 0;
 
-static uint8_t keys_shift = 0;
-static uint8_t keys_ctrl = 0;
-static uint8_t keys_caplock = 0;
+static uint8_t keys_shift_left = 0;
+static uint8_t keys_shift_right = 0;
+static uint8_t keys_ctrl_left = 0;
+static uint8_t keys_ctrl_right = 0;
+static uint8_t keys_caplock = 1;
 
-static const uint8_t keys_iie[KEYS_MATRIX_VALID] =
+static const uint8_t keys_iie[KEYS_MATRIX_VALID * 3] =
 {
+// Caps lock on (default on power up)
+
 //0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  ' ', 0x00, 0x00, 0x00, 0x00, LEFT, DOWN, RGHT, 0x00, 0x00, //0x00
   0x00,  'Z',  'X',  'C',  'V',  'B',  'N',  'M',  ',',  '.',  '/', 0x00, 0x00,   UP, 0x00, 0x00, //0x10
@@ -47,18 +61,28 @@ static const uint8_t keys_iie[KEYS_MATRIX_VALID] =
    ESC, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  DEL, 0x00, 0x00, //0x40
    '`',  '1',  '2',  '3',  '4',  '5',  '6',  '7',  '8',  '9',  '0',  '-',  '=', 0x00, 0x00, 0x00, //0x50
    TAB,  'Q',  'W',  'E',  'R',  'T',  'Y',  'U',  'I',  'O',  'P',  '[',  ']', 0x00, 0x00, 0x00, //0x60
-};
 
-static const uint8_t keys_iie_shift[KEYS_MATRIX_VALID] =
-{
+// Caps lock off
+
 //0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  ' ', 0x00, 0x00, 0x00, 0x00, LEFT, DOWN, RGHT, 0x00, 0x00, //0x00
-  0x00,  'z',  'x',  'c',  'v',  'b',  'n',  'm',  '<',  '>',  '?', 0x00, 0x00,   UP, 0x00, 0x00, //0x10
-  0x00,  'a',  's',  'd',  'f',  'g',  'h',  'j',  'k',  'l',  ':', '\"',  RTN, '|', 0x00, 0x00, //0x20
+  0x00,  'z',  'x',  'c',  'v',  'b',  'n',  'm',  ',',  '.',  '/', 0x00, 0x00,   UP, 0x00, 0x00, //0x10
+  0x00,  'a',  's',  'd',  'f',  'g',  'h',  'j',  'k',  'l',  ';', '\'',  RTN, '\\', 0x00, 0x00, //0x20
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //0x30
+   ESC, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  DEL, 0x00, 0x00, //0x40
+   '`',  '1',  '2',  '3',  '4',  '5',  '6',  '7',  '8',  '9',  '0',  '-',  '=', 0x00, 0x00, 0x00, //0x50
+   TAB,  'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',  'o',  'p',  '[',  ']', 0x00, 0x00, 0x00, //0x60
+
+// shift
+
+//0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  ' ', 0x00, 0x00, 0x00, 0x00, LEFT, DOWN, RGHT, 0x00, 0x00, //0x00
+  0x00,  'Z',  'X',  'C',  'V',  'B',  'N',  'M',  '<',  '>',  '?', 0x00, 0x00,   UP, 0x00, 0x00, //0x10
+  0x00,  'A',  'S',  'D',  'F',  'G',  'H',  'J',  'K',  'L',  ':', '\"',  RTN,  '|', 0x00, 0x00, //0x20
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //0x30
    ESC, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  DEL, 0x00, 0x00, //0x40
    '~',  '!',  '@',  '#',  '$',  '%',  '^',  '&',  '*',  '(',  ')',  '_',  '+', 0x00, 0x00, 0x00, //0x50
-   TAB,  'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',  'o',  'p',  '{',  '}', 0x00, 0x00, 0x00, //0x60
+   TAB,  'Q',  'W',  'E',  'R',  'T',  'Y',  'U',  'I',  'O',  'P',  '{',  '}', 0x00, 0x00, 0x00, //0x60
 };
 
 void keys_init(void)
@@ -95,12 +119,30 @@ void keys_update(void)
     if (key_test)
     {
         keys_ones++;
+
+        if (keys_index == KEYS_SHIFT_LEFT)
+        {
+            keys_shift_left = 1;
+        }
+
+        if (keys_index == KEYS_SHIFT_RIGHT)
+        {
+            keys_shift_right = 1;
+        }
+
         if (keys_data[keys_index] == 0)
         {
-            keys_pressed[keys_index] = key_test;
-            if (keys_index < KEYS_MATRIX_VALID)
+            if (keys_index == KEYS_CAPS_LOCK)
             {
-                keys_waiting = keys_index;
+                keys_caplock ^= 1;
+            }
+            else
+            {
+                keys_pressed[keys_index] = key_test;
+                if (keys_index < KEYS_MATRIX_VALID)
+                {
+                    keys_waiting = keys_index;
+                }
             }
         }
         keys_data[keys_index] = key_test;
@@ -108,6 +150,17 @@ void keys_update(void)
     else
     {
         keys_ones = 0;
+
+        if (keys_index == KEYS_SHIFT_LEFT)
+        {
+            keys_shift_left = 0;
+        }
+
+        if (keys_index == KEYS_SHIFT_RIGHT)
+        {
+            keys_shift_right = 0;
+        }
+
         keys_pressed[keys_index] = key_test;
         keys_used[keys_index] = key_test;
         keys_data[keys_index] = key_test;
@@ -135,19 +188,22 @@ uint8_t keys_data_waiting(void)
 uint8_t keys_data_get(void)
 {
     uint8_t key = 0;
-    // uint8_t index = 0;
-    // while (key == 0 && index < KEYS_MATRIX_VALID)
-    // {
-    //     index++;
-    //     key = keys_iie[index];
-    // }
-    // if (key)
-    // {
-    //     keys_waiting = 0;
-    //     keys_used[index] = 1;
-    // }
 
-    key = keys_iie[keys_waiting];
+    if (keys_caplock)
+    {
+        keys_iie_offset = KEYS_OFFSET_CAPS_LOCK_ON;
+    }
+    else
+    {
+        keys_iie_offset = KEYS_OFFSET_CAPS_LOCK_OFF;
+    }
+
+    if (keys_shift_left || keys_shift_right)
+    {
+        keys_iie_offset = KEYS_OFFSET_CAPS_SHIFT;
+    }
+
+    key = keys_iie[keys_waiting + keys_iie_offset];
     keys_used[keys_waiting] = 1;
     keys_waiting = 0;
     return key;
