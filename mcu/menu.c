3f65b64950fdf10b95941d02dcf45ae96b3495b6
pyrex8 <ev64bug@yahoo.com>
2023-10-22 10:29:56 -0700

    adding menu functionality

--- mcu/menu.c
+++ mcu/menu.c
@@ -7,6 +7,9 @@
 #include "hardware/sync.h"
 #include "pico/stdlib.h"
 
+#define MENU_NAME_LENGTH 24
+#define MENU_BIN_LENGTH 0xC000
+#define MENU_PAGE_LENGTH (MENU_BIN_LENGTH + MENU_NAME_LENGTH + 5)
 #define FLASH_TARGET_OFFSET (512 * 1024) // choosing to start at 512K
 
 static uint8_t menu[MENU_CHARACTERS_SIZE];
@@ -28,6 +31,22 @@ static uint8_t flash_data[40];
 static uint8_t menu_bank = 0;
 static uint8_t menu_file_name;
 
+union menu_page
+{
+   uint8_t flash_data[MENU_PAGE_LENGTH];
+   struct __attribute__((__packed__)) menu
+   {
+        uint8_t bank;
+        uint8_t name[MENU_NAME_LENGTH];
+        uint8_t file_size_lsb;
+        uint8_t file_size_msb;
+        uint8_t bin_address_lsb;
+        uint8_t bin_address_msb;
+        uint8_t bin_data[MENU_BIN_LENGTH];
+   } menu;
+} menu_page;
+
+
 void flash_data_save()
 {
     int flash_dataSize = sizeof(flash_data);
@@ -39,6 +58,25 @@ void flash_data_save()
     flash_range_erase(FLASH_TARGET_OFFSET, FLASH_SECTOR_SIZE * sectorCount);
     flash_range_program(FLASH_TARGET_OFFSET, flash_data, FLASH_PAGE_SIZE * writeSize);
     restore_interrupts(interrupts);
+}
+
+void menu_str_print(uint8_t *str, uint8_t len, uint8_t x, uint8_t y)
+{
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] != '\0')
+        {
+            menu[line[y] + x + i] = str[i] + 128;
+        }
+    }
+}
+
+void menu_hex_print(uint16_t value, uint8_t x, uint8_t y)
+{
+    menu[line[y] + x] = menu_select[(value >> 12) & 0x0F] + 128;
+    menu[line[y] + x + 1] = menu_select[(value >>  8) & 0x0F] + 128;
+    menu[line[y] + x + 2] = menu_select[(value >>  4) & 0x0F] + 128;
+    menu[line[y] + x + 3] = menu_select[(value) & 0x0F] + 128;
 }
 
 void flash_data_read()
@@ -66,6 +104,12 @@ void menu_init(void)
     menu[line[0] + 5] = flash_data[2];
 
     menu[line[23] + 3] = menu_select[menu_bank] + 128;
+
+    uint16_t value = sizeof(menu_page);
+    menu_hex_print(value, 10, 10);
+
+    uint8_t name[] = "FILE";
+    menu_str_print(name, sizeof(name), 5, 5);
 }
 
 void menu_update(void)
@@ -80,7 +124,7 @@ void menu_data_get(uint8_t *data)
 
 void menu_bank_set(uint8_t data)
 {
-    menu_bank = data;
+    menu_page.menu.bank = data;
 }
 
 void menu_name_set(uint8_t data)
