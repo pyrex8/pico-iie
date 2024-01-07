a59d891c73fcadfda80582c766cc7a0366feecaf
pyrex8 <ev64bug@yahoo.com>
2024-01-06 16:26:04 -0800

    cleaning up key.c code

--- mcu/key.c
+++ mcu/key.c
@@ -9,6 +9,7 @@
 #define KEY_MATRIX_MASK 0x7F
 #define KEY_MATRIX_TOTAL (KEY_MATRIX_MASK + 1)
 #define KEY_MATRIX_VALID (KEY_MATRIX_TOTAL - 0x10)
+#define KEY_ONES_END_OF_SCAN 16
 
 // Apple IIe keyboard codes
 #define UP   0x0B
@@ -34,18 +35,24 @@
 #define KEY_OFFSET_CAPS_LOCK_OFF 0x70
 #define KEY_OFFSET_CAPS_SHIFT    0xE0
 
+#define KEY_DATA_EMPTY 0
+#define KEY_DATA_READY 1
+#define KEY_USED_FALSE 0
+#define KEY_USED_TRUE 1
+
+#define KEY_SWITCH_CLOSED 0
+
 uint8_t key_clk_state = 1;
 uint8_t key_index = 0;
 uint8_t key_waiting = 0;
 uint8_t key_ones = 0;
-uint8_t key_data[KEY_MATRIX_TOTAL] = {0};
-uint8_t key_pressed[KEY_MATRIX_TOTAL] = {0};
-uint8_t key_used[KEY_MATRIX_TOTAL] = {0};
+uint8_t key_data[KEY_MATRIX_TOTAL] = {KEY_DATA_EMPTY};
+uint8_t key_used[KEY_MATRIX_TOTAL] = {KEY_USED_FALSE};
 uint8_t key_iie_offset = 0;
 
 static uint8_t key_shift = 0;
 static uint8_t key_ctrl = 0;
-static uint8_t key_caplock = 1;
+static uint8_t key_caplock = 0;
 static uint8_t menu_enabled = 0;
 
 static KeyOperation key_operation;
@@ -103,7 +110,7 @@ static const uint8_t key_iie[KEY_MATRIX_VALID * 3] =
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //0x7
    ' ',  ')',  '>',  '{',  'I', LEFT,  ':',  '*', //0x8
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //0x9
-  '"',  '_',  '?',  '}',  'O', RGHT,  'L',  '(', //0xA
+  '\"',  '_',  '?',  '}',  'O', RGHT,  'L',  '(', //0xA
    RTN,  '|',  'M',  '~',  'T',  DEL,  'J',  '%', //0xB
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //0xC
     UP,  '+',  ',',  'P',  'U', DOWN,  'K',  '&', //0xD
@@ -138,8 +145,7 @@ void key_update(void)
 
     key_clk_low();
 
-    // 16 zeros in a row marks the end of the keyboard matrix scan
-    uint8_t key_test = gpio_get(KEY_DATA_PIN) == 0? 1 : 0;
+    uint8_t key_test = gpio_get(KEY_DATA_PIN) == KEY_SWITCH_CLOSED? 1 : 0;
     if (key_test)
     {
         key_ones++;
@@ -188,9 +194,8 @@ void key_update(void)
             key_operation = KEY_MAIN_RESET;
         }
 
-        if (key_data[key_index] == 0)
+        if (key_data[key_index] == KEY_DATA_EMPTY)
         {
-            key_pressed[key_index] = key_test;
             if (key_index < KEY_MATRIX_VALID)
             {
                 key_waiting = key_index;
@@ -217,12 +222,11 @@ void key_update(void)
             key_caplock = 0;
         }
 
-        key_pressed[key_index] = key_test;
-        key_used[key_index] = key_test;
-        key_data[key_index] = key_test;
+        key_used[key_index] = KEY_USED_FALSE;
+        key_data[key_index] = KEY_DATA_EMPTY;
     }
 
-    if (key_ones == 16)
+    if (key_ones == KEY_ONES_END_OF_SCAN)
     {
         key_index = 0;
     }
@@ -241,7 +245,7 @@ void key_command(KeyOperation *operation, uint8_t *data)
 
 uint8_t key_data_waiting(void)
 {
-    if (key_used[key_waiting] == 0)
+    if (key_used[key_waiting] == KEY_USED_FALSE)
     {
         return key_waiting;
     }
@@ -267,7 +271,7 @@ uint8_t key_data_get(void)
     }
 
     key = key_iie[key_waiting + key_iie_offset];
-    key_used[key_waiting] = 1;
+    key_used[key_waiting] = KEY_USED_TRUE;
     key_waiting = 0;
     return key;
 }
